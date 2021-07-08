f80309147019bbb18a4bc50c180475061669829b
raghu.neer <nraghu366@gmail.com>
2021-07-08 18:20:12 +0530

    query success

--- dynamoDB/qry_db.py
+++ dynamoDB/qry_db.py
@@ -5,7 +5,30 @@ from essentials import dynamo_client, dynamo_rsc, departments
 """
 `get_item` retrieve via hash and range key is a 1:1 fit, the time it takes (hence performance) to retrieve it is limited by the hash and sharding internally.
 `query` results in a search on "all" range keys. It adds computational work, thus considered slower
+
+From the docs:
+ExecuteStatement – Retrieves a single or multiple items from a table using the PartiQL (a SQL compatible query language).
+DynamoDB also provides the BatchExecuteStatement operation, allowing you to retrieve multiple items from diffrent tables in a single operation.
+
+GetItem – Retrieves a single item from a table.
+This is the most efficient way to read a single item because it provides direct access to the physical location of the item. 
+(DynamoDB also provides the BatchGetItem operation, allowing you to perform up to 100 GetItem calls in a single operation.)
+
+Query – Retrieves all of the items that have a specific partition key.
+Within those items, you can apply a condition to the sort key and retrieve only a subset of the data.
+Query provides quick, efficient access to the partitions where the data is stored.
+
+Scan – Retrieves all of the items in the specified table.
+(This operation should not be used with large tables because it can consume large amounts of system resources.)
 """
+
+# Get count, creation time of collection, only refreshes once in 6hrs
+def tab_props(tbl: str='usrsalary', rsc_=dynamo_rsc):
+    collection = rsc_.Table(tbl)
+    return {
+        'count': collection.item_count,
+        'created_at': collection.creation_date_time
+    }
 
 # Retrieve a single item from DynamoDB
 def simple_get_item(dep: str, tbl: str='usrsalary', rsc_=dynamo_rsc):
@@ -16,8 +39,9 @@ def simple_get_item(dep: str, tbl: str='usrsalary', rsc_=dynamo_rsc):
 # query a collection
 def simple_query_tab(dep: str, tbl: str='usrsalary', rsc_=dynamo_rsc):
     collection = rsc_.Table(tbl)
-    response = collection.query(Key={'dep': dep})
-    return response['Item']
+    response = collection.query(
+        KeyConditionExpression=Key('dep').eq('dev'))
+    return response['Items']
 
 
 if __name__ == '__main__':
